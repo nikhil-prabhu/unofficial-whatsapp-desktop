db7bf832dd4f4532fd02b2c75dad6427d943291c
Nikhil Prabhu <mail@nikhilprabhu.com>
2018-06-18 14:36:08 +0530

    Fix path to index.html

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow() {
     })
     // Load the index.html file
     win.loadURL(url.format ({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'src/index.html'),
         protocol: 'file:',
         slashes: true
     }))
